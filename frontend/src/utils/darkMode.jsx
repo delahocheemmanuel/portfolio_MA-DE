e2b4ddb0ac79605b488e835e480724acef9a240c
delahocheemmanuel <delahochemanu@hotmail.fr>
2023-08-14 02:50:11 +0200

    darkmode en cours

--- frontend/src/utils/darkMode.jsx
+++ frontend/src/utils/darkMode.jsx
@@ -1,25 +1,20 @@
 import React, { useState } from 'react';
 import DarkModeToggle from 'react-dark-mode-toggle';
 
-
 const VotreComposant = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const handleDarkModeToggle = () => {
     setIsDarkMode(!isDarkMode);
-    // Ajoutez ici la logique pour changer le thème de votre application
-    // en fonction de la valeur de isDarkMode (true pour le mode sombre, false pour le mode clair).
+    document.body.classList.toggle('dark-mode'); // Ajoute ou retire la classe 'dark-mode'
   };
 
   return (
-    <div className="votre-composant">
-      {/* Ajoutez d'autres éléments de votre composant ici */}
-      <DarkModeToggle
-        onChange={handleDarkModeToggle}
-        checked={isDarkMode}
-        size={80}
-      />
-    </div>
+    <DarkModeToggle
+      onChange={handleDarkModeToggle}
+      checked={isDarkMode}
+      size={60}
+    />
   );
 };
 
